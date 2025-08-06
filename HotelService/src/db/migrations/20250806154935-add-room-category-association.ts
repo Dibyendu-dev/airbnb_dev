dc49de2a2f4b9ea0a9bb006c6f84b1e5dc13c095
ddas4548 <ddas4548@gmail.com>
2025-08-06 21:59:10 +0530

    add association table for room and romm_categories table

---
+++ HotelService/src/db/migrations/20250806154935-add-room-category-association.ts
@@ -0,0 +1,26 @@
+
+import { QueryInterface } from "sequelize";
+
+
+module.exports = {
+  async up (queryInterface:QueryInterface) {
+    await queryInterface.addConstraint('rooms',{
+      type: 'foreign key',
+      name: 'room_categories_fkey_constraint',
+      fields: ['rooms_category_id'],
+      references: {
+        table: 'room_categories',
+        field: 'id',
+      },
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+    })
+  },
+
+  async down (queryInterface:QueryInterface) {
+    await queryInterface.removeConstraint(
+      'rooms',
+      'room_categories_fkey_constraint'
+    );
+  }
+};
