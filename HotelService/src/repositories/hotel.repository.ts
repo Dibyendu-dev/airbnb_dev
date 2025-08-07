8a6182af817ad5cda5c3e21d01084d9b22e31d6e
ddas4548 <ddas4548@gmail.com>
2025-08-07 13:07:26 +0530

    implemented  a base repository class backward compatible wth repo & Service layer

--- HotelService/src/repositories/hotel.repository.ts
+++ HotelService/src/repositories/hotel.repository.ts
@@ -1,48 +1,33 @@
 import logger from "../config/logger.config";
 import Hotel from "../db/models/hotel.model";
-import { createhotelDTO } from "../dto/hotel.dto";
+
 import { NotFoundError } from "../utils/errors/app.error";
+import BaseRepository from "./base.repository";
 
-export async function createhotel(hotelData:createhotelDTO){
-    const hotel = await Hotel.create({
-        name:hotelData.name,
-        address:hotelData.address,
-        location:hotelData.location,
-        rating:hotelData.rating,
-        ratingCount:hotelData.ratingCount
 
-    })
-    logger.info(`Hotel created :${hotel.id}`)
-    return hotel
-}
 
-export async function getHotelById(id:number){
-    const hotel = await Hotel.findByPk(id)
-
-    if(!hotel){
-        logger.error(`hotel not found ${id}`)
-        throw new NotFoundError(`hotel with ${id} is not found`)
+export class HotelRepository extends BaseRepository<Hotel> {
+    constructor(){
+        super(Hotel);
     }
-    logger.info(`hotel with ${id} is  found`)
-    return hotel
-}
 
-export async function getAllHotels(){
-    const hotel = await Hotel.findAll({
-        where:{
-            deletedAt:null
+    async findAll(){
+        const hotels = await this.model.findAll({
+            where: {
+                deletedAt: null,
+            }
+        })
+
+        if (!hotels){
+            logger.error(`No hotels found`);
+            throw new NotFoundError(`No hotels found`)
         }
-    })
-    if(!hotel){
-        logger.error(`hotel not found `)
-        throw new NotFoundError(`No hotels found`)
-        
-    }
-    logger.info(`Hotel found ${Hotel.length}`)
-    return hotel
-}
 
-export async function softDeleteHotel(id:number){
+        logger.info(`Hotels found: ${hotels.length}`)
+        return hotels
+    }
+
+    async  softDelete(id:number){
     const hotel = await Hotel.findByPk(id)
 
     if(!hotel){
@@ -53,4 +38,5 @@ export async function softDeleteHotel(id:number){
     await hotel.save()
     logger.info(`Hotel soft deleted: ${hotel.id}`)
     return true
+    }
 }
--- HotelService/src/services/hotel.service.ts
+++ HotelService/src/services/hotel.service.ts
@@ -1,22 +1,24 @@
 import { createhotelDTO } from "../dto/hotel.dto";
-import { createhotel, getAllHotels, getHotelById, softDeleteHotel } from "../repositories/hotel.repository";
+import { HotelRepository } from "../repositories/hotel.repository";
+
+const hotelRepository = new HotelRepository();
 
 export async function createHotelService(hotelData:createhotelDTO){
-    const hotel = await createhotel(hotelData)
+    const hotel = await hotelRepository.create(hotelData)
     return hotel
 }
 
 export async function getHotelByIdService(id:number){
-    const hotel = await getHotelById(id)
+    const hotel = await hotelRepository.findById(id)
     return hotel
 }
 
 export async function getAllHotelsService(){
-    const hotel = getAllHotels()
+    const hotel = hotelRepository.findAll()
     return hotel
 }
 
 export async function deleteHotelService(id:number){
-    const response = softDeleteHotel(id)
+    const response = hotelRepository.softDelete(id)
     return response
 }
