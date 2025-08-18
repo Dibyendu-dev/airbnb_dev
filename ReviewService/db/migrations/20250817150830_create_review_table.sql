a5e002918d03c81f125e0660a0e8d9ccf698475f
ddas4548 <ddas4548@gmail.com>
2025-08-18 09:18:29 +0530

    added db migration for review table and implement repository layer for review service

---
+++ ReviewService/db/migrations/20250817150830_create_review_table.sql
@@ -0,0 +1,21 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE  IF NOT EXISTS reviews(
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    user_id BIGINT NOT NULL,
+    booking_id BIGINT NOT NULL,
+    hotel_id BIGINT NOT NULL,
+    comment TEXT NOT NULL,
+    rating INT NOT NULL CHECK (rating >= 1 AND rating<= 5),
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP NULL,
+    is_synced BOOLEAN NOT NULL DEFAULT FALSE
+    
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE reviews;
+-- +goose StatementEnd
